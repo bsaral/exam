753e6747c3753c702eb6730ebed64b0f91df0f19
bsaral <betul.saral@bil.omu.edu.tr>
2012-06-01 18:30:08 +0300

    exam

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exam::Application.config.secret_token = '506185ae665628b998ef5201544c593977568168d580130aa0d1face4984dba8c20ffe72d833ddc5711a5aab470f8e1548e5587ca66e7f444e52a79ed7e1e64d'
