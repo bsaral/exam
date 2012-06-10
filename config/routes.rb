99b0f6ee13fa9cd957268c67cae0f3536ad6cbed
bsaral <betul.saral@bil.omu.edu.tr>
2012-06-10 14:35:55 +0300

    düzeltme

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Exam::Application.routes.draw do
   get "sessions/new"
   resources :sessions, only: [:new, :create, :destroy]
+ 
   
   match "login" => "sessions#new",:as => "login"
   match "logout" => "sessions#destroy"
   match "deneme" => "deneme#index"
-
   get "home/index"
   root :to => "home#index"
 
