36ec9ff6c87fc5f8b42473a94022a6cec3a6eeec
bsaral <betul.saral@bil.omu.edu.tr>
2012-06-13 03:24:24 +0300

    ekleme

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -25,6 +25,7 @@ class QuestionsController < ApplicationController
   # GET /questions/new.json
   def new
     @question = Question.new
+	
 
     respond_to do |format|
       format.html # new.html.erb
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ActiveRecord::Base
   attr_accessible :answer, :question, :title
+  
 end
