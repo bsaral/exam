6a14c8a4d0655f1e8ae474302062f0ccc4007e44
bsaral <betul.saral@bil.omu.edu.tr>
2012-06-20 23:38:06 +0300

    student:finish

--- app/controllers/examinations_controller.rb
+++ app/controllers/examinations_controller.rb
@@ -2,7 +2,7 @@ class ExaminationsController < QuestionsController
 
 	def test
 	
-		@qa = Question.where(:qtype => 'a').shuffle[1..4]
+		@qa = Question.where(:qtype => 'a').shuffle[1..5]
 		@qb = Question.where(:qtype => 'b').shuffle[1..3]
 		@qc = Question.where(:qtype => 'c').shuffle[1..2]
 		@quests = @qa + @qb + @qc
@@ -12,17 +12,29 @@ class ExaminationsController < QuestionsController
 		end
 		@no = params[:no].to_i
 		
-		
-		respond_to do |format|
-			format.html # show.html.erb
-			format.json { render json: @question }
+		if @no == 10 
+			redirect_to ("/finish")
 		end
+		
+		
+		
 		
 	end
 	
 	
 	def start
+	
 		test()
 	end
+	
+	def finish
+		
+		
+		
+			
+	end
+			
+			
+	
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Exam::Application.routes.draw do
   match "/start"  => "examinations#start",  :as => "start"
   match "/test"  => "examinations#test",  :as => "test"
   match "/q/:no(/:opt)" => "examinations#test"
+  match "/finish" => "examinations#finish", :as => "finish"
 
   
 end
