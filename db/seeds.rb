c8452185c32f9196c11a621b04b76266651d1a03
bsaral <betul.saral@bil.omu.edu.tr>
2012-06-20 13:55:05 +0300

    db:seed düzeltme

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-for i in 0..10
+(0..10).each do |i|
 		q = Question.new(
 			:title 		=> "Soru-" + sprintf("%02d", i+1),
 			:qtype 		=> "aaaaabbbcc"[i % 10],
@@ -14,7 +14,7 @@ for i in 0..10
 			:answer 	=> ["A", "B", "C", "D", "A", "B", "C", "D", "A", "B"][i % 10]
                       
 		)
-		for j in 0..3
+		(0..3).each do |j|
 				q.answers.build(:content => ["AAA", "BBB", "CCC", "DDD"][j])
 		end 
 		
