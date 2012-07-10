e199d3da2f0c104adf47bd703c8f47dc2e031ba8
bsaral <betul.saral@bil.omu.edu.tr>
2012-07-10 23:05:41 +0300

    lang:start

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def set_locale
+    I18n.locale = session[:lang] || I18n.default_locale
+    { :locale => session[:lang] }
+  end
+
+  def lang
+    session[:lang] = params[:locale]
+    redirect_to request.env['HTTP_REFERER']
+  end
 end
